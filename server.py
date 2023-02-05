c01846c320778e03bc7f58b171babeddc8acd5f8
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-02-05 22:55:36 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,10 +2,9 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
-a = 1+1
 @app.route('/')
 def hello_world():
-    return render_template('index.html', test=a)
+    return render_template('index.html')
 
 @app.route("/hello")
 def hello_world():
