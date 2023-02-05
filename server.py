805260e8aca181d0ae67b02c89722e2b5b45e5ad
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-02-05 23:18:04 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask, render_template
 import random
+import requests
 
 app = Flask(__name__)
-a = random.random()
+url = "https://www.php.net/manual/ja/function.apache-response-headers.php"
+response = requests.head(url)
+print(response.headers)
+a =. response.headers
+
 @app.route('/')
 def hello_world():
     return render_template('index.html', test=a)
