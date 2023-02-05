f2c94318ad677cedb065fc666461b8864a762f2d
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-02-05 23:43:09 +0900

    Update server.py

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ import random
 import requests
 
 app = Flask(__name__)
-url = "https://www.php.net/manual/ja/function.apache-response-headers.php"
-response = requests.head(url)
-a =response.headers
+#url = "https://www.php.net/manual/ja/function.apache-response-headers.php"
+#response = requests.head(url)
+#a =response.headers
+a = 1+2
 
 @app.route('/')
 def hello_world():
