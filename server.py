ced2ba3a3dd8cd992c6bd9b322390383c49fc1b5
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-02-06 09:36:42 +0900

    Update server.py

--- server.py
+++ server.py
@@ -5,16 +5,17 @@ import requests
 
 app = Flask(__name__)
 url = "https://www.php.net/manual/ja/function.apache-response-headers.php"
-url_2 = request.url
+
 response = requests.head(url)
 a =response.headers
-b=url_2
-#response = requests.head(url_2)
-#b =response.headers
 
 @app.route('/')
 def hello_world():
+    url_2 = request.url
+    response_2 = requests.head(url_2)
+    b =response_2.headers
     return render_template('index.html', test=a, test_2=b)
+
 
 if __name__ == '__main__':
     app.run()
