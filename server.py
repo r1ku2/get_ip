0452643e9c86996865c2d3150fd7936de91319c9
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-02-06 09:02:05 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 import requests
 
 app = Flask(__name__)
 url = "https://www.php.net/manual/ja/function.apache-response-headers.php"
+url_2 = request.url
 response = requests.head(url)
 a =response.headers
-#a = 1+2
+
+response = requests.head(url_2)
+b =response.headers
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', test=a)
+    return render_template('index.html', test=a, test_2=b)
 
 if __name__ == '__main__':
     app.run()
